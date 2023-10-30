ed9aa0f04537589482a5001f10c0151ca3d4b201
Robbie Corcoran <codewithrobbie@gmail.com>
2023-10-30 16:20:29 +0100

    Test: findAll() working

--- Backend/src/test/java/com/example/cocktailcompass/cocktail/repositories/FavouriteCocktailRepositoryTest.java
+++ Backend/src/test/java/com/example/cocktailcompass/cocktail/repositories/FavouriteCocktailRepositoryTest.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import java.util.List;
+
+import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
@@ -20,7 +23,7 @@ public class FavouriteCocktailRepositoryTest {
     FavouriteCocktailRepository favouriteCocktailRepository;
 
     CocktailDTO cocktailDTO;
-    CocktailEntity storedFavouriteCocktail;
+    CocktailEntity cocktailEntity;
 
     @BeforeEach
     void setup() {
@@ -40,22 +43,60 @@ public class FavouriteCocktailRepositoryTest {
         cocktailDTO.setStrMeasure3("3 Mock Units");
 
         ModelMapper modelMapper = new ModelMapper();
-        CocktailEntity cocktailEntity = modelMapper.map(cocktailDTO, CocktailEntity.class);
-
-        storedFavouriteCocktail = favouriteCocktailRepository.save(cocktailEntity);
+        cocktailEntity = modelMapper.map(cocktailDTO, CocktailEntity.class);
     }
 
     @Test
     @Order(1)
-    @DisplayName("save()")
+    @DisplayName("save() correctly saves a FavouriteCocktail")
     void testFavouriteCocktailRepo_whenCocktailDTOIsProvided_successfullySavesFavouriteCocktail(){
-//        Act & Assert
+//        Arrange & Act
+        testEntityManager.persistAndFlush(cocktailEntity);
+        CocktailEntity storedFavouriteCocktail = cocktailEntity;
+
+        favouriteCocktailRepository.save(cocktailEntity);
+
+//        Assert
         assertEquals(
                 storedFavouriteCocktail.getIdDrink(),
                 cocktailDTO.getIdDrink(),
                 "Saved favouriteCocktail's Id is incorrect.");
     }
 
+    @Test
+    @Order(2)
+    @DisplayName("findAll()")
+    void testFavouriteCocktailRepo_whenFindAllIsCalled_shouldReturnThreeCocktails() {
+//        Arrange
+        CocktailDTO cocktailDTOTwo = new CocktailDTO();
+        cocktailDTOTwo.setIdDrink(23456);
+        cocktailDTOTwo.setStrDrink("Mocktail 2");
+        cocktailDTOTwo.setFavourite(true);
+        cocktailDTOTwo.setStrIBA("Mock Classics");
+        cocktailDTOTwo.setStrGlass("Mocktail glass");
+        cocktailDTOTwo.setStrInstructions("Mix all mock ingredients together, serve chilled.");
+        cocktailDTOTwo.setStrDrinkThumb("https://mock-url.com/drink.jpg");
+        cocktailDTOTwo.setStrIngredient1("Mock Ingredient 1");
+        cocktailDTOTwo.setStrIngredient2("Mock Ingredient 2");
+        cocktailDTOTwo.setStrIngredient3("Mock Ingredient 3");
+        cocktailDTOTwo.setStrMeasure1("1 Mock Unit");
+        cocktailDTOTwo.setStrMeasure2("2 Mock Units");
+        cocktailDTOTwo.setStrMeasure3("3 Mock Units");
+
+        ModelMapper modelMapper = new ModelMapper();
+        CocktailEntity cocktailEntityTwo = modelMapper.map(cocktailDTOTwo, CocktailEntity.class);
+
+//        Act
+        favouriteCocktailRepository.save(cocktailEntity);
+        favouriteCocktailRepository.save(cocktailEntityTwo);
+        List<CocktailEntity> savedFavouriteCocktails = favouriteCocktailRepository.findAll();
 
 
+//        Assert
+        assertEquals(
+                2,
+                savedFavouriteCocktails.size(),
+                "Returned list should contain 2 CocktailEntities."
+        );
+    }
 }
