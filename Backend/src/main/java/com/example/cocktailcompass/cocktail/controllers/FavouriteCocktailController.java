1e87bcfd32a12c60d77ff42cb7f290e4d5a545e7
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-01 17:14:03 +0100

    Refactoring

--- Backend/src/main/java/com/example/cocktailcompass/cocktail/controllers/FavouriteCocktailController.java
+++ Backend/src/main/java/com/example/cocktailcompass/cocktail/controllers/FavouriteCocktailController.java
@@ -4,7 +4,6 @@ import com.example.cocktailcompass.cocktail.exceptions.FavouriteCocktailExceptio
 import com.example.cocktailcompass.cocktail.models.dtos.CocktailDTO;
 import com.example.cocktailcompass.cocktail.sevices.CocktailService;
 import com.example.cocktailcompass.cocktail.sevices.FavouriteCocktailServiceImpl;
-import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
