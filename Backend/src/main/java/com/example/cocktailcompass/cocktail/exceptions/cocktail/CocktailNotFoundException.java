582a5faefdd925c1bb37f22ef346787c6752a5c3
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-09 15:42:34 +0100

    Set up of CocktailNotFoundException

--- Backend/src/main/java/com/example/cocktailcompass/cocktail/exceptions/cocktail/CocktailNotFoundException.java
+++ Backend/src/main/java/com/example/cocktailcompass/cocktail/exceptions/cocktail/CocktailNotFoundException.java
@@ -1,4 +1,7 @@
 package com.example.cocktailcompass.cocktail.exceptions.cocktail;
 
-public class CocktailNotFoundException {
+public class CocktailNotFoundException extends RuntimeException{
+    public CocktailNotFoundException(String message) {
+        super(message);
+    }
 }
