b52e649b0d71db30c06ec5c6811a0a6ca5397dfa
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-10 15:16:50 +0100

    Implemented SecurityConfig in line with Spring Security 6.0.11.

---
+++ Backend/src/main/java/com/example/cocktailcompass/user/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.example.cocktailcompass.user.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorizeHttpRequests) ->
+                        authorizeHttpRequests
+                                .requestMatchers("/api/user/**").permitAll().anyRequest().authenticated());
+        http.headers((headers) -> headers.defaultsDisabled().frameOptions(Customizer.withDefaults()));
+        return http.build();
+    }
+}
