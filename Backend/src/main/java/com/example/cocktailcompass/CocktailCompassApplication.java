a2e7ae39a6b60ab3144a8a4553eb98ac67b58849
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-10 15:21:52 +0100

    Inserted roles in the database using CocktailCompassApplication class

--- Backend/src/main/java/com/example/cocktailcompass/CocktailCompassApplication.java
+++ Backend/src/main/java/com/example/cocktailcompass/CocktailCompassApplication.java
@@ -1,7 +1,11 @@
 package com.example.cocktailcompass;
 
+import com.example.cocktailcompass.user.models.RoleEntity;
+import com.example.cocktailcompass.user.repositories.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CocktailCompassApplication {
@@ -10,4 +14,16 @@ public class CocktailCompassApplication {
         SpringApplication.run(CocktailCompassApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner demo(RoleRepository roleRepository){
+        return (args -> {
+            RoleEntity admin = new RoleEntity();
+            admin.setName("ROLE_ADMIN");
+            roleRepository.save(admin);
+
+            RoleEntity user = new RoleEntity();
+            admin.setName("ROLE_USER");
+            roleRepository.save(user);
+        });
+    }
 }
