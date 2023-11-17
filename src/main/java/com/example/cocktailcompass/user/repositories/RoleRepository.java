00e6adb0783f9eaa86f707caf51dd0d3f885a70e
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-17 21:20:12 +0100

    Bug fix at RoleRepo. JpaRepository was not extended and findByRoleName was expecting a String para, and not ENUM.

--- src/main/java/com/example/cocktailcompass/user/repositories/RoleRepository.java
+++ src/main/java/com/example/cocktailcompass/user/repositories/RoleRepository.java
@@ -1,11 +1,13 @@
 package com.example.cocktailcompass.user.repositories;
 
+import com.example.cocktailcompass.user.models.ERole;
 import com.example.cocktailcompass.user.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface RoleRepository {
-    Optional<Role> findByRoleName(String roleName);
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByRoleName(ERole roleName);
 }
