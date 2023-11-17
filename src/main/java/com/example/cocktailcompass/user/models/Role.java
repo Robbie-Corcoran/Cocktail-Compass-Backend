9f5cbb62bbd9f24c94e02bac6d96e2ccdc21c594
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-17 17:43:26 +0100

    Updated annotations

--- src/main/java/com/example/cocktailcompass/user/models/Role.java
+++ src/main/java/com/example/cocktailcompass/user/models/Role.java
@@ -1,17 +1,13 @@
 package com.example.cocktailcompass.user.models;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "roles")
 @Getter
 @Setter
-//Possibly need to change to @AllArgsConstructor
-@RequiredArgsConstructor
+@AllArgsConstructor
 @NoArgsConstructor
 public class Role {
 
