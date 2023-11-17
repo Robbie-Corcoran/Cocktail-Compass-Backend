ec72c22e94e77723aa45ef7dd8e0b1bd031eac4b
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-17 19:30:33 +0100

    Initialized JwtUtils for future JWT operations.

---
+++ src/main/java/com/example/cocktailcompass/user/config/jwt/JwtUtils.java
@@ -0,0 +1,11 @@
+package com.example.cocktailcompass.user.config.jwt;
+
+public class JwtUtils {
+    public boolean validateJwtToken(String jwt) {
+        return false;
+    }
+
+    public String getUsernameFromJwtToken(String jwt) {
+        return null;
+    }
+}
