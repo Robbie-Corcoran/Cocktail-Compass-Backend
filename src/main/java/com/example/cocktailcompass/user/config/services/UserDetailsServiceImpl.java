cb84f6c21554811da0d56bb6b60823ec9b08130c
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-17 18:24:20 +0100

    Implemented UserDetailsServiceImpl & loadUserByUsername logic.

--- src/main/java/com/example/cocktailcompass/user/config/services/UserDetailsServiceImpl.java
+++ src/main/java/com/example/cocktailcompass/user/config/services/UserDetailsServiceImpl.java
@@ -1,4 +1,26 @@
 package com.example.cocktailcompass.user.config.services;
 
-public class UserDetailsServiceImpl {
+import com.example.cocktailcompass.user.models.User;
+import com.example.cocktailcompass.user.models.UserDetailsImpl;
+import com.example.cocktailcompass.user.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException("Username " + username + " Not Found."));
+        return UserDetailsImpl.build(user);
+    }
 }
