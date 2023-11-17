19d842916c27d629bd55851e751b404d4e3dc911
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-17 18:17:31 +0100

    Implemented UserDetailsImpl & builder logic: Converted Set<Role> into List<GrantedAuthority>

---
+++ src/main/java/com/example/cocktailcompass/user/models/UserDetailsImpl.java
@@ -0,0 +1,80 @@
+package com.example.cocktailcompass.user.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serial;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.modelmapper.Converters.Collection.map;
+
+@Getter
+@EqualsAndHashCode(of = "id")
+public class UserDetailsImpl implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String username;
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @Builder
+    public UserDetailsImpl(Long id,
+                           String username,
+                           String email,
+                           String password,
+                           Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public static UserDetailsImpl build(User user) {
+        List<GrantedAuthority> authorities = user.getUserRoles().stream()
+            .map(role -> new SimpleGrantedAuthority(role.getRoleName().name()))
+            .collect(Collectors.toList());
+
+        return UserDetailsImpl.builder()
+                .id(user.getUserId())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .authorities(authorities)
+                .build();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
