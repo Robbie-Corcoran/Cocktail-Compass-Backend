60b291479fb74d0973e93d3741d82b01554c15a7
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-17 18:15:41 +0100

    Updated lombok for User object

--- src/main/java/com/example/cocktailcompass/user/models/User.java
+++ src/main/java/com/example/cocktailcompass/user/models/User.java
@@ -7,8 +7,12 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "users",
 uniqueConstraints = {
         @UniqueConstraint(columnNames = "username"),
