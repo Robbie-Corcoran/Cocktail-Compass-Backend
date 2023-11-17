dbb01620b5ed6e41770ab8d8f3ad1575da48db6a
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-17 17:22:41 +0100

    Implemented role and user repos.

---
+++ src/main/java/com/example/cocktailcompass/user/repositories/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.example.cocktailcompass.user.repositories;
+
+import com.example.cocktailcompass.user.models.Role;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository {
+    Optional<Role>findByRoleName(String roleName);
+}
