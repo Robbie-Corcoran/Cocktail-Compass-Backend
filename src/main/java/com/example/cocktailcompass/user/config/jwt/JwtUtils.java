546d5e0d70f3de9988474ec1a875dc46459c930c
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-17 20:20:06 +0100

    Implemented JwtUtils and logic to generate, validate and get username from Jwt.

--- src/main/java/com/example/cocktailcompass/user/config/jwt/JwtUtils.java
+++ src/main/java/com/example/cocktailcompass/user/config/jwt/JwtUtils.java
@@ -1,11 +1,65 @@
 package com.example.cocktailcompass.user.config.jwt;
 
+import com.example.cocktailcompass.user.models.UserDetailsImpl;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Component
 public class JwtUtils {
-    public boolean validateJwtToken(String jwt) {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+
+    @Value(value = "${example.app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value(value = "${example.app.jwtExpirationMs}")
+    private int jwtExpirationMs;
+    
+    public String generateJwtToken(Authentication authentication) {
+        UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+        return Jwts.builder()
+                .setSubject((userPrincipal.getUsername()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + jwtExpirationMs))
+                .signWith(key(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+
+    public String getUsernameFromJwtToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key()).build().parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key()).build().parse(authToken);
+            return true;
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
+
         return false;
     }
 
-    public String getUsernameFromJwtToken(String jwt) {
-        return null;
-    }
 }
